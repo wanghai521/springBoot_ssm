a248c9a13b06ef3f66a5f526c3c2a4705fb3b228
WangHai_1998 <wanghai.1998@qq.com>
2022-02-27 22:46:32 +0800

    集成redis，但是现在还报错，待处理

--- src/main/java/com/hai/springboot_ssm/controller/UserContrlller.java
+++ src/main/java/com/hai/springboot_ssm/controller/UserContrlller.java
@@ -2,6 +2,7 @@ package com.hai.springboot_ssm.controller;
 
 import com.hai.springboot_ssm.entity.User;
 import com.hai.springboot_ssm.service.UserService;
+import com.hai.springboot_ssm.util.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,21 +14,30 @@ import java.util.List;
  * uer控制层
  */
 @RestController
-@RequestMapping("/uercntrlller")
+@RequestMapping("/usercontrlller")
 public class UserContrlller {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisUtils redisUtils;
+
 
     @RequestMapping("/getuserbyid/{id}")
-    public String getUserById(@PathVariable int id){
-        return  userService.getUser(id).toString();
+    public String getUserById(@PathVariable int id) {
+
+        redisUtils.set("whainame", "wanghai");
+
+        String whainame = (String)redisUtils.get("whainame");
+        System.out.println(whainame);
+
+        return userService.getUser(id).toString();
     }
 
 
     @RequestMapping("/findusrsbyrole/{role}")
-    public List<User> findUsrsByRole(@PathVariable String role){
+    public List<User> findUsrsByRole(@PathVariable String role) {
 
         return userService.findUserByRole(role);
     }
